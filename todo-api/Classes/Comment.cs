d2abfb4a9e0de612882a1668cbc126607b90d15c
garyantier <gary.antier@rafi.ph>
2023-02-21 08:56:33 +0800

    chore: init

---
+++ todo-api/Classes/Comment.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace todo_api.Classes
+{
+	public class Comment
+	{
+		public int Id { get; set; }
+
+		public DateTime Timestamp { get; set; }
+
+		public string Content { get; set; }
+	}
+}
+
---
+++ todo-api/Controllers/CommentController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using todo_api.Classes;
+
+namespace todo_api.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class CommentController : ControllerBase
+{
+    private static List<Comment> _comments = new();
+
+    [HttpGet(Name = "GetComments")]
+    public IActionResult Get()
+    {
+        return Ok(_comments);
+    }
+
+    [HttpPost(Name = "CreateComments")]
+    public IActionResult Create([FromBody] Comment comment)
+    {
+        if (_comments.Any(c => c.Id == comment.Id))
+            return BadRequest($"A comment with id: {comment.Id} already exists.");
+
+        _comments.Add(comment);
+        return Ok();
+    }
+
+    [HttpPatch(Name = "UpdateComments")]
+    public IActionResult Update(int id, [FromBody] Comment newComment)
+    {
+        var comment = _comments.FirstOrDefault(c => c.Id == id);
+
+        if (comment is null)
+        {
+            return NotFound();
+        }
+
+        comment.Timestamp = newComment.Timestamp;
+        comment.Content = newComment.Content;
+
+        return Ok();
+    }
+
+    [HttpDelete(Name = "DeleteComments")]
+    public IActionResult Delete(int id)
+    {
+        var comment = _comments.FirstOrDefault(c => c.Id == id);
+
+        if (comment is null)
+        {
+            return NotFound();
+        }
+
+        _comments.Remove(comment);
+
+        return Ok();
+    }
+}
